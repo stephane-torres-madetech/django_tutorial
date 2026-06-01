43c02d784cd3d35aa55101a161007a6c47c82e3e
Stephane torres <stephane.torres@madetech.com>
2026-06-01 09:15:55 +0100

    extract tasks into separate file

--- djangotutorial/mysite/celery.py
+++ djangotutorial/mysite/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from .tasks import test_task
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
@@ -8,13 +9,17 @@ app = Celery("mysite")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
-# app.autodiscover_tasks()
+app.autodiscover_tasks()
 
 @app.on_after_finalize.connect
 def setup_periodic_tasks(sender: Celery, **kwargs):
-    sender.add_periodic_task(10, test_task.s("This a test"), name="testing")
+    sender.add_periodic_task(5, test_task.s("This a test"), name="testing")
 
-@app.task
-def test_task(message: str):
+# @app.on_after_finalize.connect
+# def setup_periodic_tasks(sender: Celery, **kwargs):
+#     sender.add_periodic_task(10, test_task.s("This a test"), name="testing")
+
+# @app.task
+# def test_task(message: str):
     
-    print(message)
+#     print(message)
